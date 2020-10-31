6516f1da644ec0aab865cc3b88e836f50af2db21
Arturo Devesa <63007186+artdevesa7@users.noreply.github.com>
2020-10-31 08:02:28 -0400

    Add files via upload

---
+++ compare_two_strings.py
@@ -0,0 +1,10 @@
+string1 = 'hello world'
+string2 = 'hello world'
+
+equalornot = string1 == string2 
+if equalornot == True:
+    print('strings are equal')
+else:
+    print('strings are not equal')
+    
+print(equalornot)
